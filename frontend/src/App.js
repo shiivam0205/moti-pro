f8ba652917526cca24a20c84992ad0ba08848047
DADAJI <st499085@gmail.com>
2026-05-04 23:23:07 +0530

    connect backend to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const API = "http://127.0.0.1:8000";
+const API = "https://moti-pro07.onrender.com/";
 
 export default function App() {
   const [msg, setMsg] = useState("");
